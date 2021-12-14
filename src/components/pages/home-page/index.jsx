3c3f06ab1c94b9bd8d58ff44406b3b4302f0fbce
Pauline Anvari <paulineanvari@gmail.com>
2021-12-13 19:56:18 -0500

    fetch data from API

--- src/components/pages/home-page/index.jsx
+++ src/components/pages/home-page/index.jsx
@@ -1,11 +1,72 @@
 import "./styles.css";
+import {useEffect, useState, useContext} from 'react';
+import {Post} from '../../post';
+import PostContext from "../../../context/post-context";
 
 export const HomePage = () => {
 
+  //use the use state hook to define the plants state
+  const [posts, setPosts] = useState([]);
+  //use the use state hook to define the loading state
+  const [loading, setLoading] = useState(true);
+
+  //use the use context hook to define the globate state using custom context
+  const globalState = useContext(PostContext);
+
+  //use the use effect hook to perform the get plant function
+  useEffect(
+    () => {
+
+      getPosts();
+    }, []
+  );
+
+  //defining the function to get plants from api
+  const getPosts = async() => {
+    try {
+      //fetch data from firebase api
+      const response = await fetch('https://firestore.googleapis.com/v1/projects/itec4012-final/databases/(default)/documents/posts/')
+      const data = await response.json();
+      //map through api to get plant data
+      const formattedData = data.documents.map((item) => {
+        return item.fields
+      });
+
+      //set the plants to the formatted data from the api
+      setPosts(formattedData); 
+      //pass the formatted data to the intialize plants function in the global context
+      globalState.initializePosts(formattedData); 
+      //set loading to false
+      setLoading(false);  
+    } catch (err) {
+      //log error if fetching from api does not work
+      console.log (err);
+      //set loading to false
+      setLoading(false);
+    }
+  }
+
+  //return page with item cards for each plant in api
   return (
     <div className="home-page">
-      <h1 className="home-title">Home</h1>
-    
+      <h1 className="home-title">Shop Our Plants</h1>
+      <div className="posts-container">
+        {
+          //map through plants and create an item card for each
+          posts.map((post) => (
+            <Post
+              //pass the component data from the api as props
+              username={post.username.stringValue} 
+              text={post.text.stringValue} 
+              image={post.image.stringValue} >
+            </Post>
+          ))
+        }
+        {
+          //define the loading state
+          loading && <div class="loader"></div>
+        }
+      </div>
     </div>
   );
 };
